cad7cffc909fa1c17bdb24796df2cd35b44f3cdb
SukhvirS <sukhvir23singh@gmail.com>
2021-12-29 12:48:56 -0800

    Updates

---
+++ precache-manifest.b3fda8ade1aa3f3d435c18002c957dc7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdc0e3c38c969d968941",
+    "url": "/static/css/main.e2937170.chunk.css"
+  },
+  {
+    "revision": "cdc0e3c38c969d968941",
+    "url": "/static/js/main.bf9be5cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "90b55baff494b10e0f13",
+    "url": "/static/js/2.f403d63d.chunk.js"
+  },
+  {
+    "revision": "8fafddc211551f128d69eb5cbcebae75",
+    "url": "/static/media/saferway1.8fafddc2.png"
+  },
+  {
+    "revision": "fef099c77f18592bbd7a8b662cdcc9d9",
+    "url": "/static/media/saferway2.fef099c7.png"
+  },
+  {
+    "revision": "207f17e64a164332140e077047addd86",
+    "url": "/static/media/saferway3.207f17e6.png"
+  },
+  {
+    "revision": "d5e0cbf8545c78331ae125a985c44af5",
+    "url": "/static/media/saferway4.d5e0cbf8.png"
+  },
+  {
+    "revision": "e8b5e9d5d45d9d859883a674192e0124",
+    "url": "/static/media/atom1.e8b5e9d5.png"
+  },
+  {
+    "revision": "f750f5066770518822d0d2a291342b54",
+    "url": "/static/media/atom2.f750f506.png"
+  },
+  {
+    "revision": "361cb312dd3896ef87a1e3cebb8d8218",
+    "url": "/static/media/atom3.361cb312.png"
+  },
+  {
+    "revision": "fe37abf6701b54b0d5ad9253d03d39ab",
+    "url": "/static/media/atom4.fe37abf6.png"
+  },
+  {
+    "revision": "d8eeb3fac9d17b7ab89fc307280aaa27",
+    "url": "/static/media/atom5.d8eeb3fa.png"
+  },
+  {
+    "revision": "8c4a1e25302c7a55e8d63f73f7ed3385",
+    "url": "/static/media/atom6.8c4a1e25.png"
+  },
+  {
+    "revision": "26f54f7f68bf2e94e535b4bff386c22c",
+    "url": "/static/media/atom7.26f54f7f.png"
+  },
+  {
+    "revision": "eac466dc8f0c9536becddeb69f8e2db8",
+    "url": "/static/media/main.eac466dc.png"
+  },
+  {
+    "revision": "0d87e6913e64a11189278400b4e0ed46",
+    "url": "/static/media/CircularStd-Light.0d87e691.otf"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "04f15c5114a76988e9db23b27eebec30",
+    "url": "/static/media/sunny-day.04f15c51.png"
+  },
+  {
+    "revision": "980d935d2e6d42dfb5fbf7db02283431",
+    "url": "/static/media/moon.980d935d.png"
+  },
+  {
+    "revision": "c83fdd1f674aca68d3491795e46b1c2a",
+    "url": "/static/media/black_arrow.c83fdd1f.png"
+  },
+  {
+    "revision": "2b6901cb2076e69421d08f66f7aa843a",
+    "url": "/static/media/white_arrow.2b6901cb.png"
+  },
+  {
+    "revision": "d97936e76fbd3f698c80fc419843aca4",
+    "url": "/static/media/grey_arrow.d97936e7.png"
+  },
+  {
+    "revision": "f06202c0700c291689914efe9e1d694b",
+    "url": "/static/media/back.f06202c0.png"
+  },
+  {
+    "revision": "63499fce609535bca59752fb11004c11",
+    "url": "/static/media/next.63499fce.png"
+  },
+  {
+    "revision": "7f7ff2018238e7570b949da4a553398c",
+    "url": "/static/media/grey_up_arrow.7f7ff201.png"
+  },
+  {
+    "revision": "ae82d2191a276e8d6f6609c5e84718a8",
+    "url": "/static/media/grey_down_arrow.ae82d219.png"
+  },
+  {
+    "revision": "ff29a61ee19838838920ef3efeadeeaf",
+    "url": "/static/media/white_up_arrow.ff29a61e.png"
+  },
+  {
+    "revision": "9b9b084c349eb9bf7f6f69cd7dbaaf47",
+    "url": "/static/media/white_down_arrow.9b9b084c.png"
+  },
+  {
+    "revision": "3ea872ad7ee844cda3e00a98874e4252",
+    "url": "/index.html"
+  }
+];
